ac4fd5cd4213ff4e6ecaa7ba83aed131930809f4
Mustafa <mjebbar17@gmail.com>
2018-02-14 14:12:18 +0000

    -added account endpoint

--- server.js
+++ server.js
@@ -18,6 +18,21 @@ app.get('/balance/:id', function(req, res){
     }
 })
 
+app.get('/account/:id', function(req, res){
+    var accountId = req.params.id;
+
+    var foundAccount = accounts.find(function(acc) {
+        return acc.id === accountId;
+    });
+
+    if(foundAccount) {
+        res.send(foundAccount);
+    }else{
+        res.send('account not found');
+    }
+})
+
+
 app.listen(3000, function(){
     console.log('Example app listening on port 3000!')
 })
