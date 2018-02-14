8281d1ff0d0f139a9470d861d0dbf7d91bf02c8e
Mustafa <mjebbar17@gmail.com>
2018-02-14 14:11:57 +0000

    -added balance endpoint

--- server.js
+++ server.js
@@ -4,6 +4,20 @@ var accounts = data.accounts;
 
 var app = express();
 
+app.get('/balance/:id', function(req, res){
+    var accountId = req.params.id;
+
+    var foundAccount = accounts.find(function(acc) {
+        return acc.id === accountId;
+    });
+
+    if(foundAccount) {
+        res.send({balance : foundAccount.balance});
+    }else{
+        res.send('account not found');
+    }
+})
+
 app.listen(3000, function(){
     console.log('Example app listening on port 3000!')
 })
